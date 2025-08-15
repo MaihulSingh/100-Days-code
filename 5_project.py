bdd6e422743989acafee0a74b7fe6b0f5f9e303e
Maihul <maihulsingh100@gmail.com>
2025-08-16 00:06:08 +0530

    Add files via upload
    Favorite Food Recommender
    A simple Python program that asks for your name and your favorite food, then gives you a fun recommendation.
    Built on Day 1 of my 100 Days of Code challenge to practice:
    
    Taking user input
    
    Storing data in variables
    
    Printing formatted output

---
+++ 5_project.py
@@ -0,0 +1,8 @@
+# Favorite Food Recommender 😋
+
+print("Welcome to the Favorite Food Recommender!")
+
+name = input("What is your name?")
+food = input("What is your favorite type of food?")
+
+print("Hello"+" " + name + "\nSince you love " + food +"\nI recommend you try it at least twice this week!") 
