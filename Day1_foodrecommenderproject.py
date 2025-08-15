8220de74345280e1f11b9eaccba33e7f4e0aac1c
Maihul <maihulsingh100@gmail.com>
2025-08-16 00:28:03 +0530

    Update Day1_foodsuggestionproject.py

--- Day1_foodrecommenderproject.py
+++ Day1_foodrecommenderproject.py
@@ -1,10 +1,11 @@
 # Favorite Food Suggestions 😋
 
-print("Welcome to the Favorite Food Recommender!")
+print("Welcome to the Favorite Food suggestions!")
 
 name = input("What is your name?")
 food = input("What is your favorite type of food?")
 
 print("Hello"+" " + name + "\nSince you love " + food +"\nI recommend you try a new recipe with it!") 
+
 
 
