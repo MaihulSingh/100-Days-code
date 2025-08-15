980a526df07bd44646597046a6fea54054a9a03b
Maihul <maihulsingh100@gmail.com>
2025-08-16 00:18:08 +0530

    Update and rename foodrecommenderproject.py to Day1_foodrecommenderproject.py

--- Day1_foodrecommenderproject.py
+++ Day1_foodrecommenderproject.py
@@ -6,3 +6,4 @@ name = input("What is your name?")
 food = input("What is your favorite type of food?")
 
 print("Hello"+" " + name + "\nSince you love " + food +"\nI recommend you try it at least twice this week!") 
+
